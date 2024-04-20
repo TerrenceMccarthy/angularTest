6a9e8ec17cad181fcd4afdff1d9a1d50213c38b3
Terrence McCarthy <terrencemccarthysnkrs@gmail.com>
2024-04-20 02:12:01 -0500

    home and products are correctly rendering

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+import { provideAnimations } from '@angular/platform-browser/animations'
+
 // allows us to use routes using provideRouter
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), provideHttpClient(),provideAnimations()]
 };
